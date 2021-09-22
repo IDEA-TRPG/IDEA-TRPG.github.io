ec02aeedc868171104f1c0fa99f75f8ea0835372
RTa-technology <scpfoundationidcardmaker@gmail.com>
2021-09-22 20:47:33 +0900

    hotfix: build

--- _book/split.py
+++ _book/split.py
@@ -2,12 +2,12 @@ import sys
 path = sys.argv[1]
 
 arr = []
-with open(path, encoding="utf-8") as f:
+with open(path) as f:
   s = f.read()
   arr = s.split("======\n")
 
 for i in range(len(arr)):
-  with open('./CHAPTER-{}.md'.format(i+2), mode='w', encoding="utf-8") as f:
+  with open('./CHAPTER-{}.md'.format(i+2), mode='w') as f:
     f.write("  \n".join(arr[i].split('\n')))
-  with open('./CHAPTER-{}.md'.format(i+2), encoding="utf-8") as f:
+  with open('./CHAPTER-{}.md'.format(i+2)) as f:
     print(f.read())
