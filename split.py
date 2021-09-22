c88a1d3ded76814b298aa3948fb8def89e2dc97c
RTa-technology <scpfoundationidcardmaker@gmail.com>
2021-09-22 21:22:50 +0900

    fix

--- split.py
+++ split.py
@@ -6,7 +6,7 @@ with open(path) as f:
   s = f.read()
   arr = s.split("======\n")
 
-print(arr[1])
 for i in range(len(arr)):
   with open('./CHAPTER-{}.md'.format(i+2), mode='w') as f:
+    print(arr[i])
     f.write("  \n".join(arr[i].split('\n')))
