dacd4a517f47317e6a50ef00bad6fc828e3b46ad
RTa-technology <scpfoundationidcardmaker@gmail.com>
2021-09-22 21:29:49 +0900

    hotfix

--- split.py
+++ split.py
@@ -4,7 +4,7 @@ path = sys.argv[1]
 arr = []
 with open(path) as f:
   s = f.read()
-  arr = s.split("======\n")
+  arr = s.split("~~~~~~\n")
 
 for i in range(len(arr)):
   with open('./CHAPTER-{}.md'.format(i+2), mode='w') as f:
