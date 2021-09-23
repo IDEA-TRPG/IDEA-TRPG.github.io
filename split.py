448340980678a8c678da1f5622cb4e4f60e7746a
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2021-09-23 16:27:49 +0900

    Update split.py

--- split.py
+++ split.py
@@ -6,8 +6,7 @@ with open(path) as f:
   s = f.read()
   arr = s.split("======\n")
 
+print(len(arr))
 for i in range(len(arr)):
   with open('./CHAPTER{}.md'.format(i+1), mode='w') as f:
     f.write("  \n".join(arr[i].split('\n')))
-  with open('./CHAPTER{}.md'.format(i+1)) as f:
-    print(f.read())
